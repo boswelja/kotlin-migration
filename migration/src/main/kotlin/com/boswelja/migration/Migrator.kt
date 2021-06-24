9fb9f456b9a952fa359476264776af13d65d7f78
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-24 20:35:58 +1200

    Add rudimentary migrate() logic

--- migration/src/main/kotlin/com/boswelja/migration/Migrator.kt
+++ migration/src/main/kotlin/com/boswelja/migration/Migrator.kt
@@ -19,6 +19,38 @@ abstract class Migrator(
     abstract suspend fun getNewVersion(): Int
 
     suspend fun migrate() {
-        // Do nothing for now
+        // Get versions
+        val oldVersion = getOldVersion()
+        val newVersion = getNewVersion()
+
+        // Build migration map
+        val migrationMap = buildMigrationMap(oldVersion, newVersion)
+
+        migrationMap.forEach { migration ->
+            migration.migrate()
+        }
+    }
+
+    /**
+     * A recursive function to build a list of [Migration]s to be run in a sequential order.
+     */
+    internal fun buildMigrationMap(
+        oldVersion: Int,
+        newVersion: Int
+    ): List<Migration> {
+        val migrations = mutableListOf<Migration>()
+        val migrationsFromOldVersion = migrations.filter { it.fromVersion == oldVersion }
+        if (migrationsFromOldVersion.count() != 1) {
+            throw IllegalArgumentException(
+                "Error getting a migration from version $oldVersion"
+            )
+        } else {
+            val migration = migrationsFromOldVersion.first()
+            migrations.add(migration)
+            if (migration.toVersion < newVersion) {
+                migrations.addAll(buildMigrationMap(migration.toVersion, newVersion))
+            }
+        }
+        return migrations
     }
 }
