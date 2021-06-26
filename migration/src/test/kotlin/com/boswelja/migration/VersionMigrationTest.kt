9b983c8cb2da8fce10802a329721ee0a2a92b5de
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-26 18:54:29 +1200

    Create VersionMigrationTest.kt

---
+++ migration/src/test/kotlin/com/boswelja/migration/VersionMigrationTest.kt
@@ -0,0 +1,34 @@
+package com.boswelja.migration
+
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import strikt.api.expectThat
+import strikt.assertions.isFalse
+import strikt.assertions.isTrue
+
+class VersionMigrationTest {
+
+    @Test
+    fun `shouldMigrate returns true if the migration can be applied`() {
+        val fromVersion = 1
+        val toVersion = 2
+        val migration = object : VersionMigration(fromVersion, toVersion) {
+            override suspend fun migrate(): Boolean = true
+        }
+
+        val shouldMigrate = runBlocking { migration.shouldMigrate(fromVersion) }
+        expectThat(shouldMigrate).isTrue()
+    }
+
+    @Test
+    fun `shouldMigrate returns false if the migration cannot be applied`() {
+        val fromVersion = 1
+        val toVersion = 2
+        val migration = object : VersionMigration(fromVersion, toVersion) {
+            override suspend fun migrate(): Boolean = true
+        }
+
+        val shouldMigrate = runBlocking { migration.shouldMigrate(toVersion) }
+        expectThat(shouldMigrate).isFalse()
+    }
+}
