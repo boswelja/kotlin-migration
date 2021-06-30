92a4870d45ba045f6be65c9b3cb2940cdb688db9
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-06-30 14:21:54 +1200

    Fix failing tests

--- migration/src/main/kotlin/com/boswelja/migration/Migrator.kt
+++ migration/src/main/kotlin/com/boswelja/migration/Migrator.kt
@@ -76,6 +76,10 @@ abstract class Migrator(
 
         // Determine next migration and add to migration map
         val migration = migrationsFromOldVersion.maxByOrNull { it.toVersion }
+        if (migration == null && fromVersion == currentVersion) {
+            // If no migrations were found, and there was no version change, return our migrations
+            return migrationMap
+        }
         checkNotNull(migration) { "Couldn't find a migration from version $fromVersion" }
 
         migrationMap.add(migration)
