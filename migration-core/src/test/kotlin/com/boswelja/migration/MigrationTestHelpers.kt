de0dfc5be14ea4564e3f816d553d98f607ccc44f
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-04 18:43:47 +1200

    Create MigrationTestHelpers.kt

---
+++ migration-core/src/test/kotlin/com/boswelja/migration/MigrationTestHelpers.kt
@@ -0,0 +1,37 @@
+package com.boswelja.migration
+
+class ConcreteVersionMigration(
+    fromVersion: Int,
+    toVersion: Int,
+    private val migrateResult: (fromVersion: Int) -> Boolean
+) : VersionMigration(fromVersion, toVersion) {
+    override suspend fun migrate(): Boolean = migrateResult(fromVersion)
+}
+
+class ConcreteConstatntMigration(
+    private val shouldMigrateResult: (fromVersion: Int) -> Boolean,
+    private val migrateResult: () -> Boolean
+) : Migration {
+    override val toVersion: Int? = null
+    override suspend fun shouldMigrate(fromVersion: Int): Boolean = shouldMigrateResult(fromVersion)
+    override suspend fun migrate(): Boolean = migrateResult()
+}
+
+fun createVersionedMigrations(
+    fromVersion: Int,
+    toVersion: Int,
+    migrateResult: (fromVersion: Int) -> Boolean
+) = (fromVersion..toVersion).map { fromVer ->
+    ConcreteVersionMigration(fromVer, fromVer + 1, migrateResult)
+}
+
+fun createConstantMigrations(
+    count: Int,
+    shouldMigrate: (index: Int) -> Boolean,
+    migrateResult: (index: Int) -> Boolean
+) = (0 until count).map { index ->
+    ConcreteConstatntMigration(
+        shouldMigrate,
+        { migrateResult(index) }
+    )
+}
