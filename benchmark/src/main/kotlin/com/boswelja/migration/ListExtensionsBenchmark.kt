93b0eb0f307e6b61a2989b5483901f661a221517
Jack Boswell (boswelja) <boswelja@outlook.com>
2021-07-04 12:02:05 +1200

    Create benchmark for list extensions

---
+++ benchmark/src/main/kotlin/com/boswelja/migration/ListExtensionsBenchmark.kt
@@ -0,0 +1,29 @@
+package com.boswelja.migration
+
+import kotlinx.benchmark.Mode
+import kotlinx.coroutines.runBlocking
+import org.openjdk.jmh.annotations.Benchmark
+import org.openjdk.jmh.annotations.BenchmarkMode
+import org.openjdk.jmh.annotations.Fork
+import org.openjdk.jmh.annotations.Measurement
+import org.openjdk.jmh.annotations.OutputTimeUnit
+import org.openjdk.jmh.annotations.Scope
+import org.openjdk.jmh.annotations.State
+import org.openjdk.jmh.annotations.Warmup
+import java.util.concurrent.TimeUnit
+
+@Warmup(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 5, time = 1, timeUnit = TimeUnit.SECONDS)
+@Fork(value = 1)
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@State(Scope.Benchmark)
+class ListExtensionsBenchmark {
+    // Create an alternating list of Booleans
+    val testList = (0 until 1000).map { it % 2 == 0 }
+
+    @Benchmark
+    fun separateBenchmark(): Unit = runBlocking {
+        testList.separate { it }
+    }
+}
