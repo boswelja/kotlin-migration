fc44d72f17295a1bc2fceb6bdf886cb55223bf29
Jack Boswell (boswelja) <boswelja@outlook.com>
2025-01-10 18:03:55 +1100

    Update AGP

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.library") version "8.6.1" apply false
+    id("com.android.library") version "8.8.0" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.1.0" apply false
     id("org.jetbrains.kotlinx.kover") version "0.9.1" apply false
     id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
